b33a93afe8fc52b4a968448c352015f7a0b99110
Igor Manukhov <m_igor97@mail.ru>
2022-07-18 14:14:29 +0300

    add params

---
+++ src/utils/params.js
@@ -0,0 +1,8 @@
+export const params = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-btn',
+  inactiveButtonClass: 'popup__save-btn_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+}
